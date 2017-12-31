931524a0d42a1d29547d1e47303093e985d983c9
Shuma Yoshioka <s64.stdio@gmail.com>
2017-12-31 17:31:26 +0900

    Implement UniversalCookies to class

--- packages/vue-universal-cookies/src/index.ts
+++ packages/vue-universal-cookies/src/index.ts
@@ -2,20 +2,27 @@ import Vue from 'vue';
 import { PluginFunction } from 'vue/types/plugin';
 import { install } from './module/install';
 import { VueConstructor } from 'vue/types/vue';
-import { CookieHandler } from './interfaces';
+import { CookieHandler, UniversalCookies } from './interfaces';
 
 export * from './browser/index';
 export * from './express/index';
 export * from './nodeHttp/index';
 
-class VueUniversalCookies {
+class VueUniversalCookies implements UniversalCookies {
 
   static install: PluginFunction<never>;
 
-  private hander: CookieHandler;
+  private handler: CookieHandler;
 
   constructor(options: VueUniversalCookies.Options) {
-    this.hander = options.handler;
+    this.handler = options.handler;
+  }
+
+  get(key: string): string | undefined {
+    return this.handler.get(key);
+  }
+  set(key: string, value: string, options: UniversalCookies.Options): void {
+    return this.handler.set(key, value, options);
   }
 
 }
