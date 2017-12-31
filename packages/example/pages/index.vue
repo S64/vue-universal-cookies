bf533769e7536a6ad71fd005a173d464a0d1a694
Shuma Yoshioka <s64.stdio@gmail.com>
2017-12-31 18:27:39 +0900

    Update example

--- packages/example/pages/index.vue
+++ packages/example/pages/index.vue
@@ -1,3 +1,9 @@
 <template>
-	<h1>Hello, World!</h1>
+	<div>
+		<h1>Example: nuxt-universal-cookies</h1>
+		{{ $cookies.set('render_time', new Date().getTime()) }}
+		<div>`$cookies.get('render_time')`: {{ JSON.stringify($cookies.get('render_time')) }}</div>
+		<div>`$cookies.get('click_time')`: {{ JSON.stringify($cookies.get('click_time')) }}</div>
+		<button v-on:click="$cookies.set('click_time', new Date().getTime())">update `click_time` by time</button>
+	</div>
 </template>
