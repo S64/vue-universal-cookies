933b4e3210afac0fdda2ff004052b8428f7927ae
Shuma Yoshioka <s64.stdio@gmail.com>
2017-12-31 21:22:13 +0900

    nuxt: Fix errors

--- packages/nuxt-universal-cookies/lib/index.js
+++ packages/nuxt-universal-cookies/lib/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = function (moduleOptions, resolve) {
   const defaults = {};
-  const options = Object.assign({}, defaults, moduleOptions, this.options);
+  const options = Object.assign({}, defaults, moduleOptions);
 
   this.addPlugin({
     src: path.resolve(__dirname, './templates/universal-cookies.plugin.ejs.js'),
